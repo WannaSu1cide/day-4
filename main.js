b9d7f127d7f13c2cafbbfa2de3c7d30ed7f5e737
WannaSu1cide <ngdanh1509@gmail.com>
2023-02-24 10:01:10 +0700

    new update

--- main.js
+++ main.js
@@ -1,14 +1,15 @@
-const img = document.querySelector(".img");
+const img = document.querySelectorAll(".img");
 const right =document.querySelector(".right");
 const left =document.querySelector(".left");
 const x =document.querySelector(".x");
 const gallery = document.querySelector(".card__galery");
 var currentIndex =0 
 
-
-for ( let i = 0; i < img.length; i++ ) {
-   console.log(img[1])
-}
+img.forEach((item,index)=>{
+    img[index].onclick = ()=>{
+        console.log(12)
+    }
+})
 
 
 
