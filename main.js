0c555442d88c133c97467e27bfe52e761013a46c
WannaSu1cide <ngdanh1509@gmail.com>
2023-02-24 09:44:49 +0700

    new update

---
+++ main.js
@@ -0,0 +1,27 @@
+const img = document.querySelector(".img");
+const right =document.querySelector(".right");
+const left =document.querySelector(".left");
+const x =document.querySelector(".x");
+const gallery = document.querySelector(".card__galery");
+var currentIndex =0 
+
+
+for ( let i = 0; i < img.length; i++ ) {
+   console.log(img[1])
+}
+
+
+
+
+left.onclick = function (){
+  
+}
+
+
+
+x.onclick = function ()
+{
+    gallery.style.opacity ="0";
+}
+
+
